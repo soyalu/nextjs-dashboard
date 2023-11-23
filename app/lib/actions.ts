afb5ade8bc9cded90bd0991daca743ec891693c0
klu <klu@ustcinfo.com>
2023-11-23 20:23:17 +0800

    Chapter16 身份验证

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -64,4 +65,18 @@ export async function deleteInvoice(id: string) {
       message: 'Database Error: Failed to Delete Invoice.',
     };
   };
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialsSignIn';
+    }
+    throw error;
+  }
 }
